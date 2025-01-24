8781298d5a92c0709decc2c56aeed3ab5270985b
kokoro2580 <hatsakon5806@gmail.com>
2025-01-24 15:55:54 +0700

    edited

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+for (int i = 1; i < N; i++) {
+        T key = d[i];
+        int j = i - 1;
+        
+        // Move elements that are smaller than key to one position ahead
+        while (j >= 0 && d[j] < key) {
+            d[j + 1] = d[j];
+            j--;
+        }
+        d[j + 1] = key;
+
+        // Display the current pass
+        cout << "Pass " << i << ":";
+        for (int k = 0; k < N; k++) {
+            cout << d[k] << " ";
+        }
+        cout << endl;
+    }
 
 }
 
