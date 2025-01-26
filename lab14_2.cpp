b48b318be60f5d26b81ef63864d92c6304338859
kokoro2580 <hatsakon5806@gmail.com>
2025-01-27 03:53:15 +0700

    edited

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,40 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+void showMatrix(const bool B[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// ฟังก์ชันสำหรับรับค่าของเมทริกซ์
+void inputMatrix(double A[N][N]) {
+    for (int i = 0; i < N; i++) { // วนลูปแต่ละแถว
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < N; j++) { // วนลูปแต่ละคอลัมน์ในแถว
+            cin >> A[i][j]; // รับค่าจากผู้ใช้แล้วเก็บในเมทริกซ์
+        }
+    }
+}
+
+void findLocalMax(const double matrix[][N], bool result[][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                result[i][j] = false;
+            } else {
+                if (matrix[i][j] >= matrix[i - 1][j] &&
+                    matrix[i][j] >= matrix[i + 1][j] &&
+                    matrix[i][j] >= matrix[i][j - 1] &&
+                    matrix[i][j] >= matrix[i][j + 1]) {
+                    result[i][j] = true;
+                } else {
+                    result[i][j] = false;
+                }
+            }
+        }
+    }
+}
